f2922e82126d7fd713bf1c8dacbcc745fff8c384
muho <hi@muho.dev>
2021-09-09 15:57:04 +0300

    create basic RecursiveComponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { createElement } from "react";
+import data from "./example.json";
+
+function RecursiveComponent({ id, el, title, children, ...rest }) {
+  return createElement(el, {
+    ...rest,
+    children:
+      typeof children === "string"
+        ? children
+        : Array.isArray(children)
+        ? children?.map((el) => <RecursiveComponent {...el} key={el.id} />)
+        : null,
+  });
+}
+
 function App() {
-  return <div></div>;
+  return <RecursiveComponent {...data} />;
 }
 
 export default App;
